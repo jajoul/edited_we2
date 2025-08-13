ea6a0a7c10332a2394b6ef24f477a363afd0b5ad
jajoul <jajoul.me@gmail.com>
2025-08-14 00:34:40 +0330

    DRF auth disabled

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -232,10 +232,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
 }
 
 
